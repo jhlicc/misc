f54be5e709ebd0357fd0a53ce695e0a77aa53eb1
Jianh Li <jhl@debian.localdomain>
2012-09-25 23:40:39 +0800

    first commit

---
+++ shift.c
@@ -0,0 +1,47 @@
+#include <string.h>
+
+char *shift(char *s, size_t n)
+{
+	char *p, c;
+	size_t ns = strlen(s);
+
+	if (!s || n > ns)
+		return NULL;
+	if (!n || n == ns)
+		return s;
+	while (n--){
+		p = s;
+		c = *p;
+		while (p != s + ns - 1){
+			*p = *(p + 1);
+			p++;
+		}
+		*p = c;
+	}
+	return s;
+}
+
+#include <stdio.h>
+int main(void)
+{
+	const char *s = "ABCDEFGHIJK";
+	size_t n = strlen(s);
+	char a[n];
+
+	strncpy(a, "ABCDEFGHIJK", n);
+	printf("%s\n", shift(a, 0));
+	strncpy(a, "ABCDEFGHIJK", n);
+	printf("%s\n", shift(a, 1));
+	strncpy(a, "ABCDEFGHIJK", n);
+	printf("%s\n", shift(a, 3));
+	strncpy(a, "ABCDEFGHIJK", n);
+	printf("%s\n", shift(a, 5));
+	strncpy(a, "ABCDEFGHIJK", n);
+	printf("%s\n", shift(a, 7));
+	strncpy(a, "ABCDEFGHIJK", n);
+	printf("%s\n", shift(a, 9));
+	strncpy(a, "ABCDEFGHIJK", n);
+	printf("%s\n", shift(a, 10));
+	strncpy(a, "ABCDEFGHIJK", n);
+	printf("%s\n", shift(a, 11));
+}
